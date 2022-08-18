06570a463cee25e6ad7abd0fbbfb5b21c67a2c1f
Rakhibul-Hasan <64598908+Rakhibul-Hasan@users.noreply.github.com>
2022-08-19 03:34:11 +0600

    email functioned

---
+++ JS/email.js
@@ -0,0 +1,13 @@
+function sendEmail(){
+    Email.send({
+        SecureToken : "11057212-562a-4632-97ad-21cfa68f417e",
+        To : 'badskull24@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : document.getElementById("subject").value,
+        Body : "Name : " + document.getElementById("name").value
+        + "<br> Email : " + document.getElementById("email").value
+        +"<br> Message : " + document.getElementById("message").value
+    }).then(
+      message => alert(message)
+    );
+}
